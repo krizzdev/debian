250653af9ea1fde0061e007a6d11b5858b43febc
Christian Stefener <christian.stefener@looxis.com>
2024-11-26 17:13:52 +0100

    fix packages file name

--- install_scripts/sway.sh
+++ install_scripts/sway.sh
@@ -23,7 +23,7 @@ read_base_packages() {
 }
 
 # Read common packages from file
-read_base_packages "$HOME/debian/install_scripts/base_packages.txt"
+read_base_packages "$HOME/debian/install_scripts/common_packages.txt"
 
 # Function to install packages if they are not already installed
 install_packages() {
